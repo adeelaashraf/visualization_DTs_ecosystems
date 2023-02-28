a7ff9047853727e86339dd66de7c533e50502f76
Adeelala <adeela-97@hotmail.com>
2023-02-28 15:42:13 +0100

    Overflow sidebar fix + Graph adjustments

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,29 +2,26 @@ import React, { useState } from "react";
 import './Sidebar.css';
 import Select from 'react-select';
 
-const options = [
-    { value: 'option1', label: 'Option 1' },
-    { value: 'option2', label: 'Option 2' },
-    { value: 'option3', label: 'Option 3' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option5', label: 'Option 5' },
-];
+// Data Types
+const data_types = ['Abiotic Data', 'Behavioural Data', 'Biotic data', 'Geometric Data', 'Historical Data', 'Landscape data', 'Metadata', 'Process Data', 'Sensor data', 'Simulation Data'];
+const vis_tech = ['3D Geometric Modeling', 'Augmented Reality', 'Bar Plot', 'Bathymetrical Map', 'Block Chart', 'Box Plot', 'Dashboard Visualization', 'Data Visualization', 'Diagram', 'Diel Plot', 'Heat Map', 'Histogram', 'Line Graph', 'Network Graph', 'Pie Chart', 'Radar Plot', 'Raster Map', 'Rose Plot', 'Sammon Map', 'Stacked Bar Plot', 'Virtual Reality'];
+const vis_tool = ['.NET', 'ARCGIS', 'Autodesk Forge', 'Autodesk Revit', 'Backbone', 'Blender', 'Bootstrap', 'COVISE', 'CityEngine', 'Creo', 'D3.js', 'Grafana', 'JQuery', 'Javascript', 'Jupyter Notebook', 'OpenScene Graph', 'Python', 'R', 'Three.js', 'Unity3D', 'Unreal Engine', 'Verge3D', 'WebGL', 'WebXR', 'Wikitude', 'jsPanel'];
 
-const options2 = [
-    { value: 'option1', label: 'Option 1' },
-    { value: 'option2', label: 'Option 2' },
-    { value: 'option3', label: 'Option 3' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option5', label: 'Option 5' },
-];
 
-const options3 = [
-    { value: 'option1', label: 'Option 1' },
-    { value: 'option2', label: 'Option 2' },
-    { value: 'option3', label: 'Option 3' },
-    { value: 'option4', label: 'Option 4' },
-    { value: 'option5', label: 'Option 5' },
-];
+const options = data_types.map((option) => ({
+    value: option,
+    label: option,
+}));
+
+const options2 = vis_tech.map((option) => ({
+    value: option,
+    label: option,
+}));
+
+const options3 = vis_tool.map((option) => ({
+    value: option,
+    label: option,
+}));
 
 function Sidebar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -66,7 +63,7 @@ function Sidebar() {
         <div className={`sidebar ${isOpen ? "open" : "closed"}`}>
             <button onClick={toggleSidebar}>Graph Options</button>
             <div className="sidebar-content">
-                <div>
+                    <br></br>
                     <Select
                         options={options}
                         isMulti={true}
@@ -75,10 +72,12 @@ function Sidebar() {
                         value={selectedOptions}
                         onChange={handleOptionChange}
                     />
-                    <label>
+                    <br></br>
+                    <label class="select_all" >
                         <input type="checkbox" checked={selectedOptions.length === options.length} onChange={handleSelectAll} />
                         Select All
                     </label>
+                    <br></br>
                     <Select
                         options={options2}
                         isMulti={true}
@@ -87,10 +86,14 @@ function Sidebar() {
                         value={selectedOptions2}
                         onChange={handleOptionChange2}
                     />
-                    <label>
+
+                    <br></br>
+                    <label class="select_all" >
                         <input type="checkbox" checked={selectedOptions2.length === options2.length} onChange={handleSelectAll2} />
                         Select All
                     </label>
+                    <br></br>
+
                     <Select
                         options={options3}
                         isMulti={true}
@@ -99,11 +102,13 @@ function Sidebar() {
                         value={selectedOptions3}
                         onChange={handleOptionChange3}
                     />
-                    <label>
+
+                    <br></br>
+                    <label class="select_all" >
                         <input type="checkbox" checked={selectedOptions3.length === options3.length} onChange={handleSelectAll3} />
                         Select All
                     </label>
-                </div>
+                    <br></br>
             </div>
         </div>
     );
